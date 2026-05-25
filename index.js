5998d0b68de110c8cdeb0fd243fd093b9885d498
tentrum911 <zaidshk11904@gmail.com>
2026-05-25 23:16:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const app = express();
 require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
@@ -40,6 +42,13 @@ client.login(process.env.TOKEN);
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("NoMercy bot is alive");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running");
+});
 app.get("/", (req, res) => {
   res.send("NoMercy bot is alive");
 });
